a0272ca98b703d08e06fb7bcf59e3585b03e2553
Hwnry <huffman.henry@ymail.com>
2017-03-22 14:49:22 -0800

    Working Webtest
    Simple login test

---
+++ WEBTEST/PartyUpWebTest/Scripts/app.js
@@ -0,0 +1,14 @@
+﻿var uri = 'http://localhost:10388/api/Account/Register';
+
+function register() {
+    var Email = $('#Email').val();
+    var Password = $('#Password').val();
+    var ConfirmPassword = $('#ConfirmPassword').val();
+
+    
+    var data = 'Email='.concat(Email, '&Password=', Password, '&ConfirmPassword=', ConfirmPassword);
+
+   // var data = 'Email=asdfjkl@yahoo.com&Password=Password@123&ConfirmPassword=Password@123';
+
+    $.post(uri, data);
+}
